76efa94339a069dd021c6302b328aa58748ad8b5
Matthew <mr.kurilovich2000@gmail.com>
2024-05-03 10:27:30 +0300

    Fix some issues

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/projects" element={<Projects />} />
-                    <Route path="/:id" element={<ProjectPage />} />
+                    <Route path="projects/:id" element={<ProjectPage />} />
                     <Route path="/contacts" element={<Contacts />} />
                 </Routes>
                 <Footer />
